ea6ffa2a1b3527bc4bf68de31767861f08f04935
Andrey <drunhouse2mail.ru>
2017-12-03 00:58:09 +0300

    nullable_then_unsigned

--- database/migrations/2017_11_25_203137_add_role_to_user_table.php
+++ database/migrations/2017_11_25_203137_add_role_to_user_table.php
@@ -14,7 +14,7 @@ class AddRoleToUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->tinyInteger('role')->unsigned();
+            $table->tinyInteger('role')->nullable();
         });
     }
 
